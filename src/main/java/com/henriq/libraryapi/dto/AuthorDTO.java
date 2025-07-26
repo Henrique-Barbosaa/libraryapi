07b8d71a544578145329606a443f34c6fdbfb2b2
Antonio Henrique <henrique1508cops@gmail.com>
2025-07-26 15:34:38 -0300

    refactor: Validação dos campos do AuthorDTO para bloquea strings muito grandes e datas inválidas.

--- src/main/java/com/henriq/libraryapi/dto/AuthorDTO.java
+++ src/main/java/com/henriq/libraryapi/dto/AuthorDTO.java
@@ -3,6 +3,8 @@ package com.henriq.libraryapi.dto;
 import com.henriq.libraryapi.model.Author;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -10,12 +12,14 @@ import java.util.UUID;
 public record AuthorDTO(
         UUID id,
         @NotBlank(message = "Campo obrigatório")
+        @Size(max = 100, message = "Campo fora do tamanho padrão")
         String name,
         @NotNull(message = "Campo obrigatório")
+        @Past(message = "Datas futuras não são permitidas nesse campo")
         LocalDate birthDate,
         @NotBlank(message = "Campo obrigatório")
+        @Size(max = 50, message = "Campo fora do tamanho padrão")
         String nationality) {
-
     public Author createAuthor(){
         Author author = new Author();
         author.setDateOfBirth(this.birthDate);
